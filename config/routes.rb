c11a74a8e691eb79f77c1128e5da40a43e492d43
Marcela Porto <marcela.fporto@gmail.com>
2017-06-10 21:20:58 -0700

    Fixes index bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to:'index'
+  root 'volunteers#index'
 
   resources :girls, except: :index
-  resources :volunteers,  except: :index
+  resources :volunteers
 
 
   get '/login', to: 'sessions#new', as: 'login'
