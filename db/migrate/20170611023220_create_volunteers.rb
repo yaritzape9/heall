58381cf7428a0fb371d57f19fbd0cdd7cf2ab051
Marcela Porto <marcela.fporto@gmail.com>
2017-06-10 21:22:07 -0700

    Adds certification to database

--- db/migrate/20170611023220_create_volunteers.rb
+++ db/migrate/20170611023220_create_volunteers.rb
@@ -3,6 +3,7 @@ class CreateVolunteers < ActiveRecord::Migration[5.1]
     create_table :volunteers do |t|
       t.string :username
       t.string :language
+      t.attachment :certification
       t.string :password_digest
 
       t.timestamps
