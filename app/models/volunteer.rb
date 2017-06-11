86f438d457c10a07570b35d40b87c0146e3a0967
Marcela Porto <marcela.fporto@gmail.com>
2017-06-10 21:22:31 -0700

    Adds validation for paperclip

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,5 +1,7 @@
 class Volunteer < ApplicationRecord
   has_secure_password
+  has_attached_file :certification
+  validates_attachment :certification, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
   has_many :volunteer_expertises
   has_many :girl_volunteers
   has_many :expertises, through: :volunteer_expertises
